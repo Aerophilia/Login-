f70bf83f6bda0e8c781d5dc57339bb35c5b364ef
Al Ashqan Babu <80443704+alashqan@users.noreply.github.com>
2022-09-08 10:52:55 +0530

    Add files via upload

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./login.css"
+const signup = () => {
+  return (
+    <section id="#register" className='register'>
+        <div className='main-container'>
+
+        <h5 className='heading'>REGISTER</h5>
+        <div className='title-head'>
+        <div className="title-individual">INDIVIDUAL</div>
+        {/* <div className="title-team">Team</div> */}
+        </div>
+        <div className="form-container">
+            <form action="#" className='individual'>
+                <div >
+                    <input type="email" placeholder='Mail ID' className='field' required/>
+                </div>
+                <div >
+                    <input type="password" placeholder='Enter password' className='field' required/>
+                </div>
+                <div className='button-container'>
+                    <input type="submit" placeholder='Submit' className="button" required/>
+                </div>
+            </form>
+            </div>        
+            </div>
+    </section>
+  )
+}
+
+export default login
